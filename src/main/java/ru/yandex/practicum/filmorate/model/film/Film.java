fc84ff3ba0ebbbf2ea028b9b71090ab18e962dab
Sergey Samoylov <sam0lov@yandex.ru>
2025-05-21 23:18:42 +0300

    HW11 Добавлен функционал добавления, удаления лайков и получения топ фильмов

--- src/main/java/ru/yandex/practicum/filmorate/model/film/Film.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/film/Film.java
@@ -2,12 +2,15 @@ package ru.yandex.practicum.filmorate.model.film;
 
 import lombok.Data;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Data
 public class Film {
-    private Integer id;
+    private Long id;
     private String name;
     private String description;
     private LocalDate releaseDate;
     private Integer duration;
+    private Set<Long> likedUserIds = new HashSet<>();
 }
